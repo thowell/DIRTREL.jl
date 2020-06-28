b803562752a7f9662449b07c984b3cd622da2a51
thowell <thowell@stanford.edu>
2020-06-28 16:06:43 -0700

    robust cost function prototype with ForwardDiff

--- dynamics/pendulum.jl
+++ dynamics/pendulum.jl
@@ -11,5 +11,10 @@ function dynamics(model::Pendulum,x,u,w)
               (u[1] - model.m*model.g*model.lc*sin(x[1]) - model.b*x[2])/model.I]
 end
 
+function dynamics(model::Pendulum,x,u,w)
+    @SVector [x[2],
+              (u[1] - model.m*model.g*model.lc*sin(x[1]) - (model.b + model.b*w[1])*x[2])/model.I]
+end
+
 n,m = 2,1
 model = Pendulum(1.0,0.1,0.5,0.25,9.81)
--- examples/pendulum.jl
+++ examples/pendulum.jl
@@ -53,10 +53,81 @@ sparsity_jacobian(prob_moi)
 @time Z_sol = solve(prob_moi,Z0)
 
 # Unpack solution
-X_sol, U_sol, H_sol = unpack(Z_sol,prob)
+X, U, H = unpack(Z_sol,prob)
 
 # Plot trajectories
 using Plots
-plot(Array(hcat(X_sol...))',width=2.0,xlabel="time step",ylabel="state",label="",title="Pendulum")
-plot(Array(hcat(U_sol...))',width=2.0,xlabel="time step",ylabel="control",label="",title="Pendulum")
-plot(Array(hcat(H_sol...))',width=2.0,xlabel="time step",ylabel="h",label="",title="Pendulum")
+plot(Array(hcat(X...))',width=2.0,xlabel="time step",ylabel="state",label="",title="Pendulum")
+plot(Array(hcat(U...))',width=2.0,xlabel="time step",ylabel="control",label="",title="Pendulum")
+plot(Array(hcat(H...))',width=2.0,xlabel="time step",ylabel="h",label="",title="Pendulum")
+
+# robust cost function
+integration = prob.integration
+idx = prob.idx
+
+# TVLQR cost
+Q_lqr = [Matrix(1.0*I,n,n) for t = 1:T]
+R_lqr = [Matrix(1.0*I,m,m) for t = 1:T-1]
+
+# robust cost
+Ql = [Matrix(I,n,n) for t = 1:T]
+Rl = [Matrix(I,m,m) for t = 1:T-1]
+
+robust_cost(Z0)
+ForwardDiff.gradient(robust_cost,Z0)
+
+function robust_cost(Z)
+    nw = 1
+    w0 = zeros(nw)
+
+    A = [zeros(eltype(Z),n,n) for t = 1:T-1]
+    B = [zeros(eltype(Z),n,m) for t = 1:T-1]
+    G = [zeros(eltype(Z),n,nw) for t = 1:T-1]
+
+    for t = 1:T-1
+        x = view(Z,idx.x[t])
+        u = view(Z,idx.u[t])
+        h = Z[idx.h[t]]
+        x⁺ = view(Z,idx.x[t+1])
+
+        dyn_x(z) = x⁺ - integration(model,z,u,w0,h)
+        dyn_u(z) = x⁺ - integration(model,x,z,w0,h)
+        dyn_w(z) = x⁺ - integration(model,x,u,z,h)
+
+        A[t] = ForwardDiff.jacobian(dyn_x,x)
+        B[t] = ForwardDiff.jacobian(dyn_u,u)
+        G[t] = ForwardDiff.jacobian(dyn_w,w0)
+    end
+
+
+    # TVLQR
+    K = [zeros(eltype(Z),m,n) for t = 1:T-1]
+    P = [zeros(eltype(Z),n,n) for t = 1:T]
+
+    P[T] = Q_lqr[T]
+    for t = T-1:-1:1
+        K[t] = (R_lqr[t] + B[t]'*P[t+1]*B[t])\(B[t]'*P[t+1]*A[t])
+        P[t] = Q_lqr[t] + A[t]'*P[t+1]*A[t] - (A[t]'*P[t+1]*B[t])*K[t]
+    end
+
+    # robust cost function
+    E1 = Matrix(I,n,n)
+
+    l = 0
+
+    E = E1
+    H = zeros(eltype(Z),n,nw)
+    D = Matrix(I,nw,nw)
+    for t = 1:T-1
+        l += tr(Ql[t] + K[t]'*Rl[t]*K[t]*E)
+        E = ((A[t] - B[t]*K[t])*E*(A[t] - B[t]*K[t])'
+            + (A[t] - B[t]*K[t])*H*G[t]'
+            + G[t]*H'*(A[t] - B[t]*K[t])'
+            + G[t]*D*G[t]')
+        H = (A[t] - B[t]*K[t])*H + G[t]*D
+    end
+
+    l += tr(Ql[t]*E)
+
+    return l
+end
